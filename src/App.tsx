1b837281c2ba7cd38f6804085ef2502ea0101cd3
Robin Lake <robinhlake@gmail.com>
2022-03-06 11:15:51 -0700

    basic graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Page from './components/Page';
 
---
+++ src/components/ForceDirectedGraph.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import * as d3 from 'd3';
+
+
+function ForceDirectedGraph() {
+
+    return (
+        <div>
+            force direccted graph
+        </div>
+    )
+}
+
+export default ForceDirectedGraph
