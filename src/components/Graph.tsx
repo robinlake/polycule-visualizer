acc54f17396fb1f364dbd2705fd0030227d813c6
Robin Lake <robinhlake@gmail.com>
2022-03-06 09:07:18 -0700

    placeholder d3 bar chart

---
+++ src/components/Graph.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Graph() {
+    return (
+        <div className="graph-container">
+            <div className="graph"></div>
+        </div>
+    )
+}
+
+export default Graph
