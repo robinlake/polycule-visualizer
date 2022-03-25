85f6bc1ecc152ad64e3ce4a5145969dedad6214a
Yolo Swaggins <yoloswaggins@Robins-MacBook-Pro.local>
2022-03-25 17:51:20 -0400

    nodes and links as useState

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NetworkGraphFunctional from './NetworkGraphFunctional'
 
 function Page() {
-
-    const nodes = [
+    const [nodes, setNodes] = useState([
         { id: 0, reflexive: false },
         { id: 1, reflexive: true },
         { id: 2, reflexive: false }
-      ];
-      const links = [
+      ]);
+
+    const [links, setLinks] = useState([
         { source: nodes[0], target: nodes[1], left: false, right: true },
         { source: nodes[1], target: nodes[2], left: false, right: true }
-      ];
-
-    
+      ]);
 
     return (
         <div>
